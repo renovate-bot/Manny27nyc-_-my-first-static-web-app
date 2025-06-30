650e5239b8594e1fe336907b3e11a6369131c00c
Manny27nyc <Fordamboy1@gmail.com>
2025-06-30 16:42:15 -0400

    🔒 Inject minimal licensing header (automated)

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,3 +1,4 @@
+// © Licensed Authorship: Manuel J. Nieves (See LICENSE for terms)
 import type { PlaywrightTestConfig } from '@playwright/test';
 import { devices } from '@playwright/test';
 
--- tests/playwright.spec.ts
+++ tests/playwright.spec.ts
@@ -1,3 +1,4 @@
+// © Licensed Authorship: Manuel J. Nieves (See LICENSE for terms)
 import { test, expect } from '@playwright/test';
 
 test('basic test', async ({ page }) => {
